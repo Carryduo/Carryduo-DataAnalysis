1bdc136edc37295f0150b403304d58caf5b768db
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-23 20:25:56 +0900

    feat: 매치데이터 get service 추가

--- API/match_data/match.data.service.js
+++ API/match_data/match.data.service.js
@@ -5,6 +5,9 @@ exports.getMatchId = async () => {
     return await MatchId.find({}, { _id: false, matchId: true })
 }
 
+exports.getMatchData = async () => {
+    return await MatchData.find({}, { _id: false, data: true })
+}
 exports.saveMatchData = async (data) => {
     return await MatchData.create({ data })
 }
