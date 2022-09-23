8695411611f79aad8ae35069009e4c8225fbf147
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-09-24 01:24:00 +0900

    feat: 챔피언조합승률 데이터분석 1차 구현 완료

--- API/match_data/index.js
+++ API/match_data/index.js
@@ -2,5 +2,7 @@ const router = require("express").Router()
 const matchDataController = require("./match.data.controller")
 
 router.get("/", matchDataController.saveMatchData)
+router.get("/combination", matchDataController.analyzeCombination)
+router.get("/combination/preview/:type", matchDataController.getAnalysis)
 
 module.exports = router
--- API/match_data/match.data.service.js
+++ API/match_data/match.data.service.js
@@ -1,6 +1,6 @@
 const MatchId = require("../../schemas/matchId")
 const MatchData = require("../../schemas/matchData")
-
+const Combination = require('../../schemas/combination.stat')
 exports.getMatchId = async () => {
     return await MatchId.find({}, { _id: false, matchId: true })
 }
@@ -10,5 +10,61 @@ exports.getMatchData = async () => {
 }
 exports.saveMatchData = async (data) => {
     return await MatchData.create({ data })
+}
+
+exports.checkCombinationData = async (mainChamp, subChamp) => {
+    return await Combination.find({ mainChampId: mainChamp.champId, subChampId: subChamp.champId })
+}
+
+exports.updateCombinationData = async (mainChamp, subChamp, category) => {
+    console.log('update', mainChamp, subChamp)
+    const data = await Combination.findOne({ mainChampId: mainChamp.champId, subChampId: subChamp.champId })
+    console.log(data)
+    let { win, lose, sampleNum } = data
+    if (category === 'win') {
+        win += 1
+    } else {
+        lose += 1
+    }
+    sampleNum += 1
+    await Combination.updateOne(
+        { mainChampId: mainChamp.champId, subChampId: subChamp.champId },
+        {
+            $set: {
+                win, lose, sampleNum
+            },
+        }
+    )
+}
+
+exports.saveCombinationData = async (mainChamp, subChamp, category, type) => {
+    console.log('save', mainChamp, subChamp)
+    if (category === 'win') {
+        await Combination.create({
+            mainChampId: mainChamp.champId,
+            mainChampName: mainChamp.champName,
+            subChampId: subChamp.champId,
+            subChampName: subChamp.champName,
+            win: 1,
+            lose: 0,
+            sampleNum: 1,
+            type
+        })
+    } else {
+        await Combination.create({
+            mainChampId: mainChamp.champId,
+            mainChampName: mainChamp.champName,
+            subChampId: subChamp.champId,
+            subChampName: subChamp.champName,
+            win: 0,
+            lose: 1,
+            sampleNum: 1,
+            type
+        })
+    }
+}
+
+exports.getData = async (type) => {
+    return await Combination.find({ type }).sort('-sampleNum').limit(5)
 }
 
