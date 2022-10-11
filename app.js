1ce81d679230d5cffcbc5c2c6ea09d4e3798b1ec
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-11 15:24:31 +0900

    build: matchId 수집용 branch 생성

--- app.js
+++ app.js
@@ -86,67 +86,67 @@ const matchIdTask = new AsyncTask(
 
 // 데이터 분석
 const job = new SimpleIntervalJob({ hours: 2, runImmediately: true }, task)
-scheduler.addSimpleIntervalJob(job)
+// scheduler.addSimpleIntervalJob(job)
 
 // 매치Id 수집 
 const matchIdJob = new SimpleIntervalJob({ hours: 12, runImmediately: true }, matchIdTask) // runImmediately: 즉시실행 
-// scheduler.addSimpleIntervalJob(matchIdJob)
+scheduler.addSimpleIntervalJob(matchIdJob)
 
 
 
-async function startAnalyze() {
-    try {
-        const start = performance.now()
+// async function startAnalyze() {
+//     try {
+//         const start = performance.now()
 
-        //데이터 분석 및 분석용 데이터베이스에 저장
-        await startChampInfo()
-        await sleep(10)
+//         //데이터 분석 및 분석용 데이터베이스에 저장
+//         await startChampInfo()
+//         await sleep(10)
 
-        // 데이터 분석 후 서비스DB에 업데이트
-        await serviceSaveRate()
-        await sleep(10)
+//         // 데이터 분석 후 서비스DB에 업데이트
+//         await serviceSaveRate()
+//         await sleep(10)
 
-        await serviceSavePosition()
-        await sleep(10)
+//         await serviceSavePosition()
+//         await sleep(10)
 
-        await serviceSaveChampSpell()
-        await sleep(10)
+//         await serviceSaveChampSpell()
+//         await sleep(10)
 
-        console.log('======챔피언조합승률 분석 시작========')
+//         console.log('======챔피언조합승률 분석 시작========')
 
-        await matchDataController.saveCombination()
-        await sleep(10)
-        await matchDataController.uploadCombinationWinRate()
-        await sleep(10)
-        await matchDataController.updateCombinationTierAndRank()
-        await sleep(10)
-        await matchDataController.transferCombinationStatToServiceDB()
+//         await matchDataController.saveCombination()
+//         await sleep(10)
+//         await matchDataController.uploadCombinationWinRate()
+//         await sleep(10)
+//         await matchDataController.updateCombinationTierAndRank()
+//         await sleep(10)
+//         await matchDataController.transferCombinationStatToServiceDB()
 
-        //데이터베이스 연결 해제
-        await db.close()
-        await db.closeService()
+//         //데이터베이스 연결 해제
+//         await db.close()
+//         await db.closeService()
 
-        //함수 실행 시간 체크
-        const end = performance.now()
-        const runningTime = end - start
-        const ConversionRunningTime = (runningTime / (1000 * 60)) % 60
-        console.log(`===${ConversionRunningTime} 분소요===`)
-    } catch (err) {
-        //에러 로그 파일
-        const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0]
-        const time = new Date().toTimeString().split(" ")[0]
-        const data = "\nerror: " + err.toString() + " ||" + " Date: " + date + " Time: " + time
+//         //함수 실행 시간 체크
+//         const end = performance.now()
+//         const runningTime = end - start
+//         const ConversionRunningTime = (runningTime / (1000 * 60)) % 60
+//         console.log(`===${ConversionRunningTime} 분소요===`)
+//     } catch (err) {
+//         //에러 로그 파일
+//         const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0]
+//         const time = new Date().toTimeString().split(" ")[0]
+//         const data = "\nerror: " + err.toString() + " ||" + " Date: " + date + " Time: " + time
 
-        fs.writeFile(
-            process.env.LOG || `./logs/champ.analyze.error.txt`,
-            data,
-            { flag: "a+" },
-            (err) => {
-                console.log(err)
-            }
-        )
-    }
-}
+//         fs.writeFile(
+//             process.env.LOG || `./logs/champ.analyze.error.txt`,
+//             data,
+//             { flag: "a+" },
+//             (err) => {
+//                 console.log(err)
+//             }
+//         )
+//     }
+// }
 
 
 async function startGetMatchIds() {
