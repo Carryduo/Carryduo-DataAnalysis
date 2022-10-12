33319f4fb7be41b297d873c933b3566934edcf79
rlasuddn <ywaves073@gmail.com>
2022-10-12 15:15:18 +0900

    docs: 데이터베이스 연결 수정 및 연결 해제 제거

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const db = require("./orm");
 
 const { ToadScheduler, SimpleIntervalJob } = require("toad-scheduler");
 
@@ -6,6 +7,9 @@ const { task, matchIdTask } = require("./schedule");
 
 const scheduler = new ToadScheduler();
 
+//데이터베이스 연결
+db.connect();
+db.connectService();
 // 데이터 분석
 const job = new SimpleIntervalJob({ hours: 1, runImmediately: true }, task);
 scheduler.addSimpleIntervalJob(job);
