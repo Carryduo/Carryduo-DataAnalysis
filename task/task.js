8c37298b05c6db5c10ad7b1f23ac566f07deb597
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-10-13 20:04:27 +0900

    분석 순서 변경

--- task/task.js
+++ task/task.js
@@ -38,20 +38,20 @@ async function startAnalyze() {
         // 데이터 분석 및 분석용 데이터베이스에 저장
         await startChampInfo()
         // 데이터 분석 후 서비스DB에 업데이트
-        await serviceSaveRate()
-        await serviceSavePosition()
-        await serviceSaveChampSpell()
-
-        console.log("======챔피언조합승률 분석 시작========")
-
-        await sleep(5)
         await matchDataController.saveCombination()
         await matchDataController.uploadCombinationWinRate()
         await matchDataController.updateCombinationTierAndRank()
-        await matchDataController.transferCombinationStatToServiceDB()
-
         await simulationController.saveSimulation()
         await simulationController.uploadSimulationWinRate()
+
+        await sleep(5)
+
+        console.log("======서비스 DB 이관========")
+
+        await serviceSaveRate()
+        await serviceSavePosition()
+        await serviceSaveChampSpell()
+        await matchDataController.transferCombinationStatToServiceDB()
         await simulationController.transferSimulationToServiceDB()
         //함수 실행 시간 체크
         const end = performance.now()
