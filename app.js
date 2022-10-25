6146b1a1c166bd2e46a0ad82ea6480db3ad9af18
rlasuddn <ywaves073@gmail.com>
2022-10-25 18:21:45 +0900

    feat: migration 생성

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const db = require("./orm")
+const ServiceDB = require("./orm")
 
 const { ToadScheduler, SimpleIntervalJob } = require("toad-scheduler")
 
@@ -9,7 +10,7 @@ const scheduler = new ToadScheduler()
 
 //데이터베이스 연결
 db.connect()
-db.connectService()
+ServiceDB.connectService()
 // // 데이터 분석
 const job = new SimpleIntervalJob({ hours: 1, runImmediately: true }, task)
 scheduler.addSimpleIntervalJob(job)
