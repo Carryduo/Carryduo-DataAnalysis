c891b7587502e15ef98e1f38222c321d58e5935d
rlasuddn <ywaves073@gmail.com>
2022-10-28 12:47:03 +0900

    docs: ban rate rate.controller 로 이동

--- analyze/champ/champ.rate/rate.controller.js
+++ analyze/champ/champ.rate/rate.controller.js
@@ -5,13 +5,25 @@ const {
     allWinRateVersion,
     rateInfo,
     saveWinPickRate,
+    createBanCnt,
+    updateBanCnt,
+    deleteBanOldVersion,
+    getBanVersion,
+    allBanVersion,
+    banInfo,
+    saveBanRate,
 } = require("./rate.service")
 
 const { successAnalyzed } = require("../champInfo.service")
 
 const logger = require("../../../log")
 
-exports.rate = async (data, key) => {
+/**
+ * win rate
+ * win rate save
+ */
+
+exports.winRate = async (data, key) => {
     try {
         let analyzedOption
         console.log(
@@ -53,12 +65,12 @@ exports.rate = async (data, key) => {
         }
         await successAnalyzed(matchId, analyzedOption)
     } catch (err) {
-        logger.error(err, { message: "- from rate" })
+        logger.error(err, { message: "- from winRate" })
         return
     }
 }
 
-exports.saveWinRate = async () => {
+exports.winPickRateSave = async () => {
     try {
         let dupWinRateVersion = []
         const WinRateAllVersion = await allWinRateVersion()
@@ -94,6 +106,115 @@ exports.saveWinRate = async () => {
         return "승/픽률 데이터 서비스 table 업데이트 완료"
     } catch (err) {
         logger.error(err, { message: "- from saveWinPickRate" })
+
+        return err
+    }
+}
+
+/**
+ * ban rate
+ * ban rate save
+ * pick rate save
+ */
+
+exports.banRate = async (data, key) => {
+    try {
+        console.log(
+            `=============================================밴 카운팅 ${key}번============================================`
+        )
+        const matchId = data.metadata.matchId
+        const version = data.info.gameVersion.substring(0, 5)
+        let champList = []
+
+        const teams = data.info.teams
+        for (let t of teams) {
+            const ban = t.bans
+
+            for (let b of ban) {
+                const champId = b.championId
+
+                if (champId === -1) {
+                    console.log(champId)
+                    continue
+                }
+                champList.push(champId)
+            }
+        }
+
+        const setChampList = new Set(champList)
+        const newChampLIst = [...setChampList]
+
+        let option
+        let banCount
+
+        for (let nCL of newChampLIst) {
+            const versionCheck = await getBanVersion(nCL, version)
+            if (!versionCheck) {
+                // create
+                if (champList.indexOf(nCL) === champList.lastIndexOf(nCL)) {
+                    banCount = 1
+                    await createBanCnt(nCL, banCount, version)
+                } else if (champList.indexOf(nCL) !== champList.lastIndexOf(nCL)) {
+                    banCount = 2
+                    await createBanCnt(nCL, banCount, version)
+                }
+            } else if (versionCheck) {
+                //update
+                if (champList.indexOf(nCL) === champList.lastIndexOf(nCL)) {
+                    option = {
+                        set: { banCount: () => "banCount+1", sampleNum: () => "sampleNum+1" },
+                    }
+                    await updateBanCnt(nCL, option, version)
+                } else if (champList.indexOf(nCL) !== champList.lastIndexOf(nCL)) {
+                    option = {
+                        set: { banCount: () => "banCount+2", sampleNum: () => "sampleNum+1" },
+                    }
+                    await updateBanCnt(nCL, option, version)
+                }
+            }
+        }
+        await deleteBanOldVersion()
+        analyzedOption = {
+            set: { banAnalyzed: 1 },
+        }
+        await successAnalyzed(matchId, analyzedOption)
+    } catch (err) {
+        logger.error(err, { message: "- from banRate" })
+        return
+    }
+}
+
+exports.banRateSave = async () => {
+    try {
+        let dupBanVersion = []
+        const banAllVersion = await allBanVersion()
+
+        for (let bI of banAllVersion) {
+            dupBanVersion.push(bI.version)
+        }
+
+        const set = new Set(dupBanVersion)
+        const uniqBanVersion = [...set]
+
+        for (let bV of uniqBanVersion) {
+            if (bV === "old") {
+                continue
+            }
+            const banInfos = await banInfo(bV)
+            for (let bIs of banInfos) {
+                const champId = bIs.champId
+                const sampleNum = bIs.sampleNum
+                const version = bIs.version
+                const banCount = bIs.banCount
+
+                let banRate = (banCount / sampleNum) * 100
+                banRate = Number(banRate.toFixed(2))
+                await saveBanRate(champId, banRate, version)
+            }
+        }
+        return "벤 데이터 서비스 table 업데이트 완료"
+    } catch (err) {
+        logger.error(err, { message: "- from saveBanRate" })
 
         return err
     }
--- analyze/champ/champ.rate/rate.service.js
+++ analyze/champ/champ.rate/rate.service.js
@@ -1,7 +1,9 @@
 const { dataSource } = require("../../../orm")
 const ChampWinRate = dataSource.getRepository("champ_win_rate")
+const ChampBan = dataSource.getRepository("champban")
 const ChampService = dataSource.getRepository("champ_service")
 
+//rate
 exports.getRateVersion = async (champId, version) => {
     return await ChampWinRate.createQueryBuilder()
         .where("champId = :champId", { champId })
@@ -54,6 +56,65 @@ exports.saveWinPickRate = async (champId, winRate, pickRate, version) => {
         await ChampService.createQueryBuilder()
             .update(ChampService)
             .set({ win_rate: winRate, pick_rate: pickRate })
+            .where("champId = :champId", { champId })
+            .andWhere("version = :version", { version })
+            .execute()
+    }
+}
+
+//ban
+exports.createBanCnt = async (champId, banCount, version) => {
+    return ChampBan.createQueryBuilder()
+        .insert()
+        .values({ champId, banCount, version, sampleNum: 1 })
+        .execute()
+}
+
+exports.updateBanCnt = async (champId, option, version) => {
+    return ChampBan.createQueryBuilder()
+        .update(ChampBan)
+        .set(option.set)
+        .where("champId = :champId", { champId })
+        .andWhere("version = :version", { version })
+        .execute()
+}
+
+exports.deleteBanOldVersion = async () => {
+    return ChampBan.createQueryBuilder()
+        .delete()
+        .where("version NOT IN (:...version)", { version: ["12.20", "12.19"] })
+        .execute()
+}
+
+exports.getBanVersion = async (champId, version) => {
+    return ChampBan.createQueryBuilder()
+        .where("champId = :champId", { champId })
+        .andWhere("version = :version", { version })
+        .getOne()
+}
+
+exports.allBanVersion = async () => {
+    return ChampBan.createQueryBuilder("ban").select("ban.version").getMany()
+}
+
+exports.banInfo = async (version) => {
+    return ChampBan.createQueryBuilder().where("version = :version", { version }).getMany()
+}
+
+exports.saveBanRate = async (champId, banRate, version) => {
+    const check = await ChampService.createQueryBuilder()
+        .where("champId = :champId", { champId })
+        .andWhere("version = :version", { version })
+        .getOne()
+    if (!check) {
+        await ChampService.createQueryBuilder()
+            .insert()
+            .values({ champId, ban_rate: banRate, version })
+            .execute()
+    } else if (check) {
+        await ChampService.createQueryBuilder()
+            .update(ChampService)
+            .set({ ban_rate: banRate })
             .where("champId = :champId", { champId })
             .andWhere("version = :version", { version })
             .execute()
