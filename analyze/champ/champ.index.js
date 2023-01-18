320900f42a4917f8b5d1719bb90e92c912381ae5
rlasuddn <ywaves073@gmail.com>
2023-01-19 00:27:18 +0900

    fix: champ 데이터 분석 코드 로직 수정

--- analyze/champ/champ.index.js
+++ analyze/champ/champ.index.js
@@ -2,11 +2,17 @@ const { sleep } = require("../../timer/timer")
 const logger = require("../../log")
 const axios = require("axios")
 
-const { matchIdList, saveMatchIdVersion, dropAnalyzed } = require("./champ.common.service")
-const { winRate, banRate, winPickRateCalculation, banRateCalculation } = require("./champ.rate/rate.controller")
+const {
+    matchIdList,
+    saveMatchIdVersion,
+    dropAnalyzed,
+    createOrIncreaseGameCount,
+    createOrUpdateChampRate,
+    createOrUpdateChampBan,
+    createOrUpdateChampSpell,
+    successAnalyzed,
+} = require("./champ.common.service")
 
-const { position, positionCalculation } = require("./champ.position/position.controller")
-const { spell, spellCaculation } = require("./champ.spell/spell.controller")
 const { rateDataToService, spellDataToService } = require("./champ.service/data.save.controller")
 
 let key = 0
@@ -14,25 +20,52 @@ let status
 exports.startChampDataSave = async () => {
     try {
         const data = await matchIdList()
-        logger.info(data.length, { message: "- 승/밴/픽, 스펠, 포지션 데이터분석 matchId 개수" })
+        logger.info(data.length, {
+            message: "- 승/밴/픽, 스펠, 포지션 데이터분석 matchId 개수",
+        })
         while (key !== data.length) {
             if (status !== undefined) {
                 status = undefined
                 continue
             }
-            const matchData = await requestRiotAPI(data[key].matchid_matchId)
-            if (matchData === "next" || matchData === "drop") {
+            const riotResponse = await requestRiotAPI(data[key]?.matchid_matchId)
+            if (riotResponse === "next" || riotResponse === "drop") {
                 // console.log("비정상 matchData skip")
+                key++
                 continue
-            } else if (matchData === 403) {
-                return
+            } else if (riotResponse === "expire") {
+                key = data.length
+                continue
             }
-            await position(matchData, key)
-            await winRate(matchData, key)
-            await banRate(matchData, key)
-            await spell(matchData, key)
+            const { participants } = riotResponse.matchData.info
+            const { version } = riotResponse
+            await createOrIncreaseGameCount(version)
+            for (let v of participants) {
+                const champId = v.championId
+                const win = v.win
+                const spell1 = v.summoner1Id
+                const spell2 = v.summoner2Id
+                const position = v.teamPosition
 
+                await createOrUpdateChampRate(champId, win, position, version)
+                await createOrUpdateChampSpell(champId, spell1, spell2, position, version)
+            }
+            //ban
+            const { teams } = riotResponse.matchData.info
+            const champList = []
+            for (let t of teams) {
+                const { bans } = t
+                for (let b of bans) {
+                    const champId = b.championId
+                    if (champId === -1) {
+                        continue
+                    }
+                    await createOrUpdateChampBan(champId, version)
+                    champList.push(champId)
+                }
+            }
             key++
+            await successAnalyzed(data[key]?.matchid_matchId)
         }
         key = 0
         logger.info("승/밴/픽, 스펠, 포지션 데이터분석완료")
@@ -48,17 +81,19 @@ async function requestRiotAPI(matchId) {
     try {
         const matchDataApiUrl = `https://asia.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${process.env.KEY}`
         const response = await axios.get(matchDataApiUrl)
+
         const matchData = response.data
 
         if (matchData.info.gameMode !== "CLASSIC" && matchData.info.queueId !== 420) {
             await dropAnalyzed(matchId)
-            key++
+            // console.log("게임속성 부적절")
             return "next"
         }
 
         const version = matchData.info.gameVersion.substring(0, 5)
         await saveMatchIdVersion(matchId, version)
-        return matchData
+
+        return { matchData, version }
     } catch (err) {
         if (!err.response) {
             logger.error(err, { message: key + " 번째 부터 오류!" })
@@ -70,11 +105,12 @@ async function requestRiotAPI(matchId) {
             return
         } else if (err.response.status === 403) {
             logger.error(err, { message: key + "api키 갱신 필요!" })
-            return 403
+            return "expire"
         } else {
-            logger.error(err, { message: `- from requestRiotAPI matchId: ${matchId} ` })
+            logger.error(err, {
+                message: `- from requestRiotAPI matchId: ${matchId} `,
+            })
             status = err.response.status
-            key++
             return "drop"
         }
     }
